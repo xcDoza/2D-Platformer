edcef7e3cc720fdb90b592e356c6c30017824f8a
Luis Santamaria(Doza) <xc2doza@hotmail.com>
2024-10-04 16:35:56 -0400

    :tada: Begin a project 2D Platformer

---
+++ scripts/Player.gd
@@ -0,0 +1,66 @@
+extends KinematicBody2D
+
+var gravity = 1000
+var velocity = Vector2.ZERO
+var maxHorizontalSpeed = 150
+var horizontalAcceleration = 2000
+var jumpSpeed = 350
+var jumpTerminationMultiplier = 4
+var hasDoubleJump = false
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	var moveVector = get_movement_vector()
+	velocity.x += moveVector.x * horizontalAcceleration * delta
+	if (moveVector.x == 0):
+		velocity.x = lerp(0, velocity.x, pow(2,-50 * delta))
+		
+	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
+	
+	if (moveVector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped() || hasDoubleJump)):
+		velocity.y = moveVector.y * jumpSpeed
+		if(!is_on_floor() && $CoyoteTimer.is_stopped()):
+			hasDoubleJump = false
+		$CoyoteTimer.stop()
+		
+	if(velocity.y < 0 && !Input.is_action_pressed("saltar")):
+		velocity.y += gravity * jumpTerminationMultiplier * delta
+	else:
+		velocity.y += gravity * delta
+	
+	var wasOnFloor = is_on_floor()
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if(wasOnFloor && !is_on_floor()):
+		$CoyoteTimer.start()
+	
+	if(is_on_floor()):
+		hasDoubleJump = true
+	
+	update_animation()
+
+
+func get_movement_vector():
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("derecha") - Input.get_action_strength("izquierda") 
+	moveVector.y = -1 if Input.is_action_just_pressed("saltar") else 0
+	return moveVector
+
+func update_animation():
+	var moveVector = get_movement_vector()
+	
+	if(!is_on_floor()):
+		$AnimatedSprite.play("jump")
+	elif (moveVector.x != 0):
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+	
+	if(moveVector.x != 0):
+		$AnimatedSprite.flip_h = true if moveVector.x > 0 else false
+
+func _on_CoyoteTimer_timeout():
+	pass # Replace with function body.
