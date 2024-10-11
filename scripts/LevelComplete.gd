d6cb467e39aabfa619267abee2a78df78993fd50
xcDoza <xc2doza@hotmail.com>
2024-10-11 16:09:59 -0400

    :sparkles: add camera shake

--- scripts/LevelComplete.gd
+++ scripts/LevelComplete.gd
@@ -5,4 +5,4 @@ func _ready():
 	$PanelContainer/MarginContainer/VBoxContainer/Button.connect("pressed", self, "on_next_button_pressed")
 
 func on_next_button_pressed():
-	$"/root/LevelManager".increment_level()
+		$"/root/LevelManager".increment_level()
