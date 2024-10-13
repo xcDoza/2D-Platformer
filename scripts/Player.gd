219a7501fadba38cdee4b45f4e5af4ab8f5103f1
xcDoza <xc2doza@hotmail.com>
2024-10-13 17:33:37 -0400

    :sparkles: complete player death animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -129,9 +129,9 @@ func kill():
 		return
 	isDying = true
 	var playerDeathInstance = playerDeathScene.instance()
+	playerDeathInstance.velocity = velocity
 	get_parent().add_child_below_node(self,playerDeathInstance)
 	playerDeathInstance.global_position = global_position
-	playerDeathInstance.velocity = velocity
 	emit_signal("died")
 
 func on_hazard_area_entered(_area2d):
--- scripts/PlayerDeath.gd
+++ scripts/PlayerDeath.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 var gravity = 1000
 
+func _ready():
+	if (velocity.x > 0):
+		$Visuals.scale = Vector2(-1, 1)
+
 func _process(delta):
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
